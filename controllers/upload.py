bd99eeba60014d0fa323b73c2173214898bcd0e5
Mariam Mahmoud <146382004+MariamMahmoud2003@users.noreply.github.com>
2026-05-16 13:20:54 +0300

    Update upload.py

--- controllers/upload.py
+++ controllers/upload.py
@@ -18,10 +18,16 @@ from database.database import SessionLocal
 from models import models
 from schemas import schemas
 
+from ml.model import model  # your loaded keras model
+from ml.gradcam import generate_gradcam, save_gradcam_overlay
+import numpy as np
+
 from core.dependencies import (
     get_db,
     get_current_user
 )
+
+import cv2
 
 router = APIRouter(
     prefix="/history",
@@ -30,8 +36,7 @@ router = APIRouter(
 
 
 @router.post(
-    "/upload",
-    response_model=schemas.HistoryOut
+    "/upload"
 )
 def upload_history(
 
@@ -95,12 +100,29 @@ def upload_history(
         )
     disease, confidence = predict_oct(file_path)
 
+    # ===================== NEW: GRAD-CAM =====================
+    # preprocess for gradcam (same style as model input)
+    img = cv2.imread(file_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (256, 256))
+    img = img.astype("float32") / 255.0
+    img = np.expand_dims(img, axis=-1)
+    img = np.expand_dims(img, axis=0)
+
+    # auto-detect last conv layer
+    heatmap = generate_gradcam(model, img)
+
+    base_name, _ = os.path.splitext(file_path)
+    gradcam_path = f"{base_name}_gradcam.jpg"
+
+    save_gradcam_overlay(file_path, heatmap, gradcam_path)
+
     # ---------------- CREATE HISTORY ----------------
 
     history = models.History(
 
         image_url=file_path,
-
+        gradcam_url=gradcam_path,
         # doctor from JWT
         doctor_id=current_user.id,
 
@@ -122,6 +144,7 @@ def upload_history(
         "status": "success",
         "diagnosis": disease,
         "confidence": confidence,
-        "history_id": history.id
+        "history_id": history.id,
+        "gradcam_url": gradcam_path
     }
 
